a02c03dff924f9d20328164c6499f40080f83012
Nesia <helena.maslowska17@gmail.com>
2023-06-06 17:39:00 +0200

    add custom images

--- app/src/main/java/com/example/myfashion/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myfashion/ui/home/HomeFragment.kt
@@ -17,10 +17,8 @@ import java.io.File
 class HomeFragment : Fragment() {
 
 private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
+  // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View
     {
         // VIEW SET UP ###########################################################################
